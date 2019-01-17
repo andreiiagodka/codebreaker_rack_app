6293a2ffc47139ce75f83de366bed433ae19e177
andrei_iagodka <andrei.iagodka@gmail.com>
2019-01-17 15:45:38 +0200

    Add validation on empty params at authentication

--- config.ru
+++ config.ru
@@ -2,5 +2,6 @@ require_relative 'autoload'
 
 use Rack::Reloader
 use Rack::Static, urls: ['/assets'], root: './'
+use Rack::Session::Cookie, key: 'rack.session', secret: 'password'
 
 run CodebreakerWeb
--- lib/entities/codebreaker_web.rb
+++ lib/entities/codebreaker_web.rb
@@ -5,6 +5,8 @@ class CodebreakerWeb
 
   def initialize(env)
     @request = Rack::Request.new(env)
+    @post = @request.params
+    @errors = []
   end
 
   def response
@@ -12,6 +14,8 @@ class CodebreakerWeb
     when '/' then return Rack::Response.new(render('menu'))
     when '/rules' then return Rack::Response.new(render('rules'))
     when '/statistics' then return Rack::Response.new(render('statistics'))
+    when '/game' then return Rack::Response.new(render('game'))
+    when '/authenticate' then return authentication
     end
   end
 
@@ -19,7 +23,32 @@ class CodebreakerWeb
     Codebreaker::Statistic.new.load_statistics
   end
 
+  def authentication
+    if @post['player_name'].empty? || @post['level'].empty?
+      @request.session[:errors] = @errors << I18n.t('error.player_name_length',
+        min_length: Codebreaker::Player::NAME_LENGTH_RANGE.min,
+        max_length: Codebreaker::Player::NAME_LENGTH_RANGE.max)
+      redirect('/')
+    end
+  end
+
+  def errors?
+    @request.session.key?(:errors)
+  end
+
+  def output_errors
+    @request.session[:errors]
+  end
+
+  def clear_session
+    @request.session.clear
+  end
+
   private
+
+  def redirect(route)
+    Rack::Response.new { |response| response.redirect(route) }
+  end
 
   def render(template)
     path = File.expand_path("../../views/#{template}.html.haml", __FILE__)
