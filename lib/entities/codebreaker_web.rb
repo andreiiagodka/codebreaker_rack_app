d4d0c816a22f6cc69cb3fa2d699573bdfbd9dd58
andreiiagodka <andrei.iagodka@gmail.com>
2019-01-21 22:52:31 +0200

    Split routes into active game mode and inactive

--- lib/entities/codebreaker_web.rb
+++ lib/entities/codebreaker_web.rb
@@ -1,32 +1,65 @@
 class CodebreakerWeb
+  ROUTES = {
+    index: '/',
+    game: '/game',
+    win: '/win',
+    lose: '/lose',
+    rules: '/rules',
+    statistics: '/statistics',
+    registration: '/registration',
+    hint: '/hint',
+    guess: '/guess'
+  }.freeze
+
   def self.call(env)
     new(env).response.finish
   end
 
   def initialize(env)
     @request = Rack::Request.new(env)
+    @route = @request.path
+    @session = @request.session
     @post = @request.params
     @errors = []
   end
 
   def response
-    case @request.path
-    when '/' then return Rack::Response.new(render('menu'))
-    when '/rules' then return Rack::Response.new(render('rules'))
-    when '/statistics' then return Rack::Response.new(render('statistics'))
-    when '/win' then return Rack::Response.new(render('win'))
-    when '/lose' then return Rack::Response.new(render('lose'))
-    when '/game' then return gameflow
-    when '/authentication' then return authentication
-    when '/guess' then return guess_result
-    when '/use_hint' then return use_hint
+    session_present?(:game) ? active_mode : inactive_mode
+  end
+
+  def active_mode
+    case @route
+    when ROUTES[:game] then return Rack::Response.new(render('game'))
+    when ROUTES[:hint] then hint
+    when ROUTES[:guess] then guess
+    when ROUTES[:win] then win
+    when ROUTES[:lose] then lose
+    else redirect(ROUTES[:game])
     end
   end
 
-  def guess_result
-    unless @request.session.key?(:game)
-      redirect('/')
+  def inactive_mode
+    case @route
+    when ROUTES[:index] then return Rack::Response.new(render('menu'))
+    when ROUTES[:rules] then return Rack::Response.new(render('rules'))
+    when ROUTES[:statistics] then return Rack::Response.new(render('statistics'))
+    when ROUTES[:registration] then registration
+    else redirect(ROUTES[:index])
     end
+  end
+
+  def win
+    return redirect(ROUTES[:game]) unless game_inactive?
+    Codebreaker::Statistic.new.save(@request.session[:player], @request.session[:game])
+    return Rack::Response.new(render('win'))
+  end
+
+  def lose
+    return redirect(ROUTES[:game]) unless game_inactive?
+    return Rack::Response.new(render('lose'))
+  end
+
+  def guess
     @guess = Codebreaker::Guess.new(@post['guess_code'])
     validate_entity(@guess)
     unless @errors.empty?
@@ -34,8 +67,14 @@ class CodebreakerWeb
       return redirect('/game')
     end
     @game = @request.session[:game]
-    return redirect('/win') if @game.win?(@guess.guess_code)
-    return redirect('/lose') if @game.loss?
+    if @game.win?(@guess.guess_code)
+      @request.session[:game_inactive] = true
+      return redirect('/win')
+    end
+    if @game.loss?
+      @request.session[:game_inactive] = true
+      return redirect('/lose')
+    end
     @game.increment_used_attempts
     @request.session[:marked_guess] = @game.mark_guess(@guess.guess_code)
     redirect('/game')
@@ -45,15 +84,7 @@ class CodebreakerWeb
     Codebreaker::Statistic.new.load_statistics
   end
 
-  def gameflow
-    unless @request.session.key?(:game)
-      redirect('/')
-    end
-    return Rack::Response.new(render('game'))
-  end
-
-  def use_hint
-    return redirect('/') unless @request.session.key?(:game)
+  def hint
     return redirect('/game') if hints_available?
     @game = @request.session[:game]
     @request.session[:hints] = [] unless session_present?(:hints)
@@ -61,7 +92,7 @@ class CodebreakerWeb
     redirect('/game')
   end
 
-  def authentication
+  def registration
     @player = Codebreaker::Player.new(@post['player_name'])
     validate_entity(@player)
     @difficulty = Codebreaker::Difficulty.new(@post['level'])
@@ -102,6 +133,9 @@ class CodebreakerWeb
 
   private
 
+  def game_inactive?
+    @session[:game_inactive] == true
+  end
 
   def validate_entity(entity)
     entity.validate
